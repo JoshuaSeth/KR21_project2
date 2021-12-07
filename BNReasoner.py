43ce7edb86c7891a26be5f30a95ecc7bf8680517
JoshuaSeth <SB@accette.eu>
2021-12-07 13:46:24 +0100

    moved cpt factor multiplication to reasoner

--- BNReasoner.py
+++ BNReasoner.py
@@ -1,5 +1,7 @@
 from typing import Union
 from BayesNet import BayesNet
+import copy
+import pandas as pd
 
 
 class BNReasoner:
@@ -14,45 +16,87 @@ class BNReasoner:
             self.bn.load_from_bifxml(net)
         else:
             self.bn = net
-            
+
     def d_separation(self, network, x, y, z):
-            z_parents = BN.get_parents(z)
-            z_children = BN.get_children(z)
-            print(z_children)
-            print(z_parents)
-            nodes_to_visit = [(x, 'asc')]
-            already_visited = set()
-            nodes = set(BN.get_all_variables())
+        z_parents = BN.get_parents(z)
+        z_children = BN.get_children(z)
+        print(z_children)
+        print(z_parents)
+        nodes_to_visit = [(x, 'asc')]
+        already_visited = set()
+        nodes = set(BN.get_all_variables())
 
-            while nodes_to_visit:
-                (node_name, up_or_down) = nodes_to_visit.pop()
+        while nodes_to_visit:
+            (node_name, up_or_down) = nodes_to_visit.pop()
 
-                if (node_name, up_or_down) not in already_visited: # if current visiting node is not already_visited, skip it
-                    already_visited.add((node_name, up_or_down))
+            # if current visiting node is not already_visited, skip it
+            if (node_name, up_or_down) not in already_visited:
+                already_visited.add((node_name, up_or_down))
 
-                    if node_name not in z and node_name == y: # if we reach the end, no d-separation
-                        return False
+                if node_name not in z and node_name == y:  # if we reach the end, no d-separation
+                    return False
 
-                    if up_or_down == 'asc' and node_name not in z:
-                        for parent in z_parents:
-                            nodes_to_visit.append((parent, 'asc'))
+                if up_or_down == 'asc' and node_name not in z:
+                    for parent in z_parents:
+                        nodes_to_visit.append((parent, 'asc'))
+                    for child in z_children:
+                        nodes_to_visit.append((child, 'des'))
+                elif up_or_down == 'des':
+                    if node_name not in z:
                         for child in z_children:
                             nodes_to_visit.append((child, 'des'))
-                    elif up_or_down == 'des':
-                        if node_name not in z:
-                            for child in z_children:
-                                nodes_to_visit.append((child, 'des'))
-                        if node_name in z or node_name in z_parents:
-                            for parents in z_parents:
-                                nodes_to_visit.append((parents, 'asc'))
-            return True
-        
+                    if node_name in z or node_name in z_parents:
+                        for parents in z_parents:
+                            nodes_to_visit.append((parents, 'asc'))
+        return True
+
+    def multiply_cpts(self, cpt_1, cpt_2):
+        '''Given 2 probability tables multiplies them and returns the multiplied CPT'''
+        # 1. get variables that is in 2nd cpt and not in 1st
+        cpt_1_no_p = list(cpt_1)[:-1]
+        vars_to_add = [col for col in list(
+            cpt_2) if col not in cpt_1_no_p]
+
+        # 2. Construct new CPT
+        new_cpt_cols = cpt_1_no_p + vars_to_add
+        index_cols_of_first_cpt = len(cpt_1_no_p)
+        new_cpt_len = pow(2, len(new_cpt_cols)-1)
+        new_cpt = pd.DataFrame(columns=new_cpt_cols, index=range(new_cpt_len))
+
+        # 3. Fill in CPT with Trues and falses
+        for i in range(len(new_cpt_cols)-1):
+            rows_to_fill_in = pow(2, len(new_cpt_cols)-2-i)
+            cur_bool = False
+            for j in range(int(new_cpt_len/rows_to_fill_in)):
+                start_i = j * rows_to_fill_in
+                cur_bool = not cur_bool
+                new_cpt[new_cpt_cols[i]][start_i:start_i +
+                                         rows_to_fill_in] = cur_bool
+
+        # 4. Get the rows in the current CPTs that correspond to values and multiply their p's
+        for index, row in new_cpt.iterrows():
+            cols = list(new_cpt)[:-1]
+            p_1 = copy.deepcopy(cpt_1)
+            p_2 = copy.deepcopy(cpt_2)
+
+            index_1 = 0
+            for col in cols:
+                if col in list(cpt_1):
+                    p_1 = p_1.loc[p_1[col] == row[index_1]]
+                if col in list(cpt_2):
+                    p_2 = p_2.loc[p_2[col] == row[index_1]]
+                index_1 += 1
+            result = float(p_1["p"].item()) * float(p_2["p"].item())
+            new_cpt["p"][index] = result
+
+        return new_cpt
+
+
 reasoner = BNReasoner('testing/dog_problem.BIFXML')
 BN = BayesNet()
 network = BN.load_from_bifxml('testing/dog_problem.BIFXML')
 BN.draw_structure()
-test= reasoner.d_separation(network, 'family-out', 'hear-bark', ['dog-out'])
+test = reasoner.d_separation(network, 'family-out', 'hear-bark', ['dog-out'])
 print(test)
 
-            
-    # TODO: This is where your methods should go
+# TODO: This is where your methods should go
--- temp_marginal_dist.py
+++ temp_marginal_dist.py
@@ -1,50 +1,11 @@
 from BayesNet import BayesNet
 import pandas as pd
 import copy
+from BNReasoner import BNReasoner
 
 BN = BayesNet()
 BN.load_from_bifxml('testing/dog_problem.BIFXML')
-
-
-def multiply_cpts(cpt_1, cpt_2):
-    # 1. get variables that is in 2nd cpt and not in 1st
-    cpt_1_no_p = list(cpt_1)[:-1]
-    vars_to_add = [col for col in list(
-        cpt_2) if col not in cpt_1_no_p]
-
-    # 2. Construct new CPT
-    new_cpt_cols = cpt_1_no_p + vars_to_add
-    index_cols_of_first_cpt = len(cpt_1_no_p)
-    new_cpt_len = pow(2, len(new_cpt_cols)-1)
-    new_cpt = pd.DataFrame(columns=new_cpt_cols, index=range(new_cpt_len))
-
-    # 3. Fill in CPT with Trues and falses
-    for i in range(len(new_cpt_cols)-1):
-        rows_to_fill_in = pow(2, len(new_cpt_cols)-2-i)
-        cur_bool = False
-        for j in range(int(new_cpt_len/rows_to_fill_in)):
-            start_i = j * rows_to_fill_in
-            cur_bool = not cur_bool
-            new_cpt[new_cpt_cols[i]][start_i:start_i +
-                                     rows_to_fill_in] = cur_bool
-
-    # 4. Get the rows in the current CPTs that correspond to values and multiply their p's
-    for index, row in new_cpt.iterrows():
-        cols = list(new_cpt)[:-1]
-        p_1 = copy.deepcopy(cpt_1)
-        p_2 = copy.deepcopy(cpt_2)
-
-        index_1 = 0
-        for col in cols:
-            if col in list(cpt_1):
-                p_1 = p_1.loc[p_1[col] == row[index_1]]
-            if col in list(cpt_2):
-                p_2 = p_2.loc[p_2[col] == row[index_1]]
-            index_1 += 1
-        result = float(p_1["p"].item()) * float(p_2["p"].item())
-        new_cpt["p"][index] = result
-
-    return new_cpt
+BR = BNReasoner(BN)
 
 
 def get_marginal_distribution(Q, E):
@@ -61,9 +22,9 @@ def get_marginal_distribution(Q, E):
     # 1.2 Multiply them into 1 big CPT
 
 
-cpt_1 = BN.get_cpt("light-on")
+cpt_1 = BN.get_cpt("hear-bark")
 cpt_2 = BN.get_cpt("dog-out")
 print(cpt_1)
 print(cpt_2)
-t = multiply_cpts(cpt_1, cpt_2)
+t = BR.multiply_cpts(cpt_1, cpt_2)
 print(t)
